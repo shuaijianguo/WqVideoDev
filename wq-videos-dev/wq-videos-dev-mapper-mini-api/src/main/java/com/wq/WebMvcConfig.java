27890f8f5c03d748e17219157483346305481c96
wuqingvika <526288022@qq.com>
2018-07-22 14:32:15 +0800

    修改了基本配置文件 静态访问目录映射

--- wq-videos-dev/wq-videos-dev-mapper-mini-api/src/main/java/com/wq/WebMvcConfig.java
+++ wq-videos-dev/wq-videos-dev-mapper-mini-api/src/main/java/com/wq/WebMvcConfig.java
@@ -13,7 +13,9 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/META-INF/resources/")//swagger-ui是放在静态目录下的
-                .addResourceLocations("file:D:/wqlesson/userData/");//本地虚拟目录映射
+				    // .addResourceLocations("file:D:/wqlesson/userData/");//本地虚拟目录映射
+        .addResourceLocations("file:E:\\wuqingGitHub\\userData\\");
+        
         //http://localhost:8081/180701HKMX8GA2CH/face/wx5dadb7c2be0c857b.o6zAJsyRSIo7y30m1iGp-km6Q2mE.7xQUJEzkGG3pa1cb19e6ef75b3330dfe5c378ce13967.jpg
     }
 }
