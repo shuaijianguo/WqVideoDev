13ce9d4a1644146e02e1fa38642e8e548d1fa0ba
wuqingvika <526288022@qq.com>
2018-07-29 22:34:34 +0800

    前端搜索页配置 加载热搜的实现

--- wq-wx-front/app.js
+++ wq-wx-front/app.js
@@ -2,6 +2,11 @@
 App({
  // serverUrl:"http://192.168.1.107:8081",
   serverUrl: "http://10.10.100.95:8081",
- userInfo:null
-  
+ userInfo:null,
+ setGlobalUserInfo:function(userInfo){
+   wx.setStorageSync("userInfo",userInfo);
+ },
+  getGlobalUserInfo:function(){
+    return wx.getStorageInfoSync("userInfo");
+  }
 })
